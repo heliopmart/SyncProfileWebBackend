411ee15484395a1c98bd22e885026a1aeee7c16e
Hélio Martins <helioperesmartinsneto@gmail.com>
2025-02-01 20:29:23 -0300

    remove dev dotenv

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
@@ -24,7 +24,7 @@ const serviceAccount = {
     universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
 }
 
-console.log(process.env)
+console.log(serviceAccount.env)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
