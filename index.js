acfa5871f8db763f977f8989b32c46293b11337f
Hélio Martins <helioperesmartinsneto@gmail.com>
2025-02-01 21:27:34 -0300

    trying fix auth token problem

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const serviceAccount = {
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -23,9 +23,6 @@ const serviceAccount = {
     client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
     universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
 }
-
-console.log(process.env.FIREBASE_PRIVATE_KEY)
-console.log(process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'))
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
